1b4c3b51d2ac006933218b9182edc5a5bf50cf4f
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-08-01 10:53:49 +0530

    Date Time Module

---
+++ 12PythonModules/demoModules.py
@@ -0,0 +1,19 @@
+name = 'Sara'
+
+age = 27
+
+print('------------------------------------')
+
+def add(a, b):
+    print(a, ' + ', b, ' = ', a + b)
+    
+    
+'''
+1. No Return Type with out Argument
+
+2. No Return Type with Argument
+
+3. Return Type with out Argument
+
+4. Return Type with Argument
+'''
---
+++ 12PythonModules/project01.py
@@ -0,0 +1,11 @@
+import demoModules
+
+print(demoModules.name, ' your age is ', demoModules.age)
+
+print('------------------------------------------')
+
+demoModules.add(10, 2)
+
+demoModules.add(20, 2)
+
+demoModules.add(30, 2)
