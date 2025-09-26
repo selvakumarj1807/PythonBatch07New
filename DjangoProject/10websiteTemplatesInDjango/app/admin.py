d4434b4a0abd77611aa85955d4ac7bbf6d74db10
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-09-26 11:19:33 +0530

    Make Appoitment

--- DjangoProject/10websiteTemplatesInDjango/app/admin.py
+++ DjangoProject/10websiteTemplatesInDjango/app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Doctor
+from .models import Appoitment, Doctor
 
 admin.site.register(Doctor)
+
+admin.site.register(Appoitment)
--- DjangoProject/10websiteTemplatesInDjango/app/views.py
+++ DjangoProject/10websiteTemplatesInDjango/app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from app.forms import AppoitmentForm
 from app.models import Doctor
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # Handle appointment form submission
+    if request.method == "POST":
+        form = AppoitmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirect to homepage or success page
+    else:
+        form = AppoitmentForm()
+
+    doctors = Doctor.objects.filter(status=True)  # Get active doctors
+    context = {
+        'form': form,
+        'doctors': doctors
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
